8e875692826b627233a5d18a127526a1261b382e
Adrian Camilo Vasquez Rincón <adrivazri23@gmail.com>
2021-12-13 23:19:40 -0500

    celular

--- src/components/servicio.vue
+++ src/components/servicio.vue
@@ -25,7 +25,7 @@
     <div class="marco1">
       <img src="../assets/imagenesHogar/marco.png" alt="" />
       <div class="cuadroSer">
-        <img src="../assets/imagenesHogar/cuadrado.png" alt="" />
+        <img class="cuadradoCambiar"  src="../assets/imagenesHogar/cuadrado.png" alt="" />
         <router-link to="/servicioAtencion">
           <h3 id="atencion">Servicio Atención</h3></router-link
         >
@@ -135,5 +135,100 @@ export default {
   padding-right: 7px;
   font-family: "Fredoka One", cursive;
   letter-spacing: 2px;
+}
+@media screen and (max-width: 320px) {
+  .cuadroCambiar{
+    width: 20px;
+    height: 30px;
+    position: absolute;
+  }
+  .servicios {
+    text-align: center;
+    margin-top: 80px;
+    font-size: 40px;
+    color: rgba(0, 0, 0, 0.61);
+    font-family: "Fredoka One", cursive;
+  }
+  .fondo {
+    position: fixed;
+    width: 322px;
+    height: 700px;
+    background-image: url("../assets/imagenesHogar/Fondo_solo_ 3.png");
+  }
+  .marco1 {
+    position: absolute;
+    width: 40px;
+    left: 5px;
+    top: 145px;
+  }
+  .marco2 {
+    position: absolute;
+    width: 40px;
+    left: 5px;
+    top: 245px;
+  }
+  .cuadroSer {
+    position: absolute;
+    width: 40px;
+    left: 15px;
+    top: 242px;
+  }
+  .cuadroSer2 {
+    position: absolute;
+    left: 35px;
+    top: 242px;
+    margin-left: 1px;
+  }
+
+  #atencion {
+    position: absolute;
+    left: 22px;
+    top: 35px;
+    font-size: 32px;
+    text-align: center;
+    font-family: "Raleway", sans-serif;
+    color: #000000;
+  }
+  #formacion {
+    position: absolute;
+    left: 22px;
+    top: 35px;
+    font-size: 32px;
+    text-align: center;
+    font-family: "Raleway", sans-serif;
+    color: #000000;
+  }
+  .inicioServicio {
+    position: absolute;
+    width: 150px;
+    height: 60px;
+    left: 10px;
+    top: 5px;
+    padding-left: 7px;
+    background: #266439;
+    border-radius: 25px;
+  }
+  .iconocasa {
+    float: left;
+  }
+  .iniciosesionServicio {
+    position: absolute;
+    width: 230px;
+    height: 60px;
+    left: 1110px;
+    top: 5px;
+    padding-left: 5px;
+    background: #266439;
+    border-radius: 25px;
+  }
+  .iconopersona {
+    float: left;
+  }
+  .letrasesionServicio {
+    color: white;
+    padding-right: 7px;
+    font-family: "Fredoka One", cursive;
+    letter-spacing: 2px;
+  }
 }
 </style>
